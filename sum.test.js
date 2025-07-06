c13fdd517140ed999eb1fb4e388bbcf84290f36b
Ramesh Das <124421675+rameshdas37@users.noreply.github.com>
2025-07-06 15:37:12 +0530

    Update sum.test.js
    corrected the code

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,3 @@
-const sum = (a, b) => a + b;
-test('adds 1 + 2 to equal 4', () => {
-  expect(sum(1, 2)).toBe(4); // Intentional failure: 1 + 2 != 4
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
